19c58f211fff6dea03fa283efcd9d79d3483cbdb
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-06 03:42:31 +0800

    feat(monorepo): :sparkles: add lib functions with tests, fix shared `tailwind` config and integration

--- packages/config-tailwind/src/index.js
+++ packages/config-tailwind/src/index.js
@@ -1,8 +1,3 @@
-import aspectRatio from "@tailwindcss/aspect-ratio";
-import containerQueries from "@tailwindcss/container-queries";
-import forms from "@tailwindcss/forms";
-import typography from "@tailwindcss/typography";
-
 /**
  * A shared TailwindCSS configuration for the repository.
  * @type {import("tailwindcss").Config}
@@ -10,5 +5,4 @@ import typography from "@tailwindcss/typography";
 export const baseTailwindConfig = {
   content: ["**/*.{ts,tsx}"],
   darkMode: "class",
-  plugins: [typography, forms, aspectRatio, containerQueries],
 };
