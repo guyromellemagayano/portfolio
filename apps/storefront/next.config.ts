7006fecd35bcef42df49766b7b9ffdbdfeafab49
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-05 15:36:42 +0800

    fix: :bug: UI package integration and style configs for `storefront` app

--- apps/storefront/next.config.ts
+++ apps/storefront/next.config.ts
@@ -4,9 +4,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: [
     "@guyromellemagayano/components",
+    "@guyromellemagayano/logger",
     "@guyromellemagayano/ui",
   ],
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 export default nextConfig;
