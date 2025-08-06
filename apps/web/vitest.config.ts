7d1adfecf041150334e8605cda1eaf9eea143320
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-07 06:29:57 +0800

    test(web): :white_check_mark: fix missing `vitest` `react` plugin integration

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,3 +1,4 @@
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vitest/config";
@@ -9,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@web": path.resolve(__dirname, "./src"),
