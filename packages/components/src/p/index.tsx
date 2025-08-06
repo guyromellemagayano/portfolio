33ae6a78aa83ce1fa94c38a399e0bb3da248e2d2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-07 05:50:30 +0800

    fix(components): :bug: fix typo issues on code exports for `P` component

---
+++ packages/components/src/p/index.tsx
@@ -0,0 +1,49 @@
+import React, { Suspense } from "react";
+
+import type { CommonComponentProps } from "../types";
+
+const PClient = React.lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.PClient };
+});
+const MemoizedPClient = React.lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.MemoizedPClient };
+});
+
+export type PRef = React.ComponentRef<"p">;
+
+export interface PProps
+  extends React.ComponentPropsWithoutRef<"p">,
+    CommonComponentProps {}
+
+/**
+ * Render the paragraph server component.
+ */
+export const P = React.forwardRef<PRef, PProps>((props, ref) => {
+  const {
+    as: Component = "p",
+    isClient = false,
+    isMemoized = false,
+    children,
+    ...rest
+  } = props;
+
+  const element = <Component {...rest}>{children}</Component>;
+
+  if (isClient) {
+    const ClientComponent = isMemoized ? MemoizedPClient : PClient;
+
+    return (
+      <Suspense fallback={element}>
+        <ClientComponent {...rest} ref={ref}>
+          {children}
+        </ClientComponent>
+      </Suspense>
+    );
+  }
+
+  return element;
+});
+
+P.displayName = "P";
