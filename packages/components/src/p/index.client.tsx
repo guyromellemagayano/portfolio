e5b337063bedf9be6100422eb143cd164ac05bc5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-07 05:50:30 +0800

    fix(components): :bug: fix typo issues on code exports for `P` component

---
+++ packages/components/src/p/index.client.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { memo } from "react";
+
+import { P, type PProps, type PRef } from ".";
+
+/**
+ * Render the paragraph client component.
+ */
+export const PClient = React.forwardRef<PRef, PProps>((props, ref) => (
+  <P ref={ref} {...props} />
+));
+
+PClient.displayName = "PClient";
+
+/**
+ * Memoized version of `PClient` for performance optimization.
+ */
+export const MemoizedPClient = memo(PClient);
