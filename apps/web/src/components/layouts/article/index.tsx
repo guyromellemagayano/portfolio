d014dab63cdacfcb16907ff184fa250a39943026
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-07 14:04:55 +0800

    test(web): :white_check_mark: add missing tests in `ArticleLayout` component, update SVG code

--- apps/web/src/components/layouts/article/index.tsx
+++ apps/web/src/components/layouts/article/index.tsx
@@ -11,6 +11,7 @@ import {
   Header,
   Heading,
   Span,
+  Svg,
   SvgProps,
   Time,
 } from "@guyromellemagayano/components";
@@ -25,14 +26,14 @@ interface ArrowLeftIconProps extends SvgProps {}
 
 const ArrowLeftIcon = (props: ArrowLeftIconProps) => {
   return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+    <Svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
       <path
         d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </Svg>
   );
 };
 
