d9b612ef11a67964a3c9eee521584e2d257a777a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-08 05:39:01 +0800

    feat: :sparkles: add articles page

---
+++ apps/web/src/app/articles/page.tsx
@@ -0,0 +1,84 @@
+import type { Metadata } from "next";
+
+import { Article, Div } from "@guyromellemagayano/components";
+
+import { Card, SimpleLayout } from "@web/components";
+import { type ArticleWithSlug, formatDate, getAllArticles } from "@web/lib";
+
+const articleStrings = {
+  read: "Read article",
+};
+
+interface ArticleSingleProps {
+  article: ArticleWithSlug;
+}
+
+/**
+ * A single article component
+ */
+const ArticleSingle = (props: ArticleSingleProps) => {
+  const { article } = props;
+
+  const articleLink = `/articles/${article.slug}`;
+
+  const element = (
+    <Article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        {article.title && article.slug && (
+          <Card.Title href={articleLink}>{article.title}</Card.Title>
+        )}
+
+        {article.date && (
+          <Card.Eyebrow dateTime={article.date} className="md:hidden" decorate>
+            {formatDate(article.date)}
+          </Card.Eyebrow>
+        )}
+
+        {article.description && (
+          <Card.Description>{article.description}</Card.Description>
+        )}
+
+        {article.slug && <Card.Cta>{articleStrings.read}</Card.Cta>}
+      </Card>
+
+      {article.date && (
+        <Card.Eyebrow dateTime={article.date} className="mt-1 max-md:hidden">
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+      )}
+    </Article>
+  );
+
+  return element;
+};
+
+Article.displayName = "Article";
+
+export const metadata: Metadata = {
+  title: "Articles",
+  description:
+    "All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.",
+};
+
+const pageContent = {
+  title:
+    "Writing on software design, company building, and the aerospace industry.",
+  intro:
+    "All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.",
+};
+
+export default async function ArticlesPage() {
+  let articles = await getAllArticles();
+
+  return (
+    <SimpleLayout title={pageContent.title} intro={pageContent.intro}>
+      <Div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <Div className="flex max-w-3xl flex-col space-y-16">
+          {articles.map((article) => (
+            <ArticleSingle key={article.slug} article={article} />
+          ))}
+        </Div>
+      </Div>
+    </SimpleLayout>
+  );
+}
