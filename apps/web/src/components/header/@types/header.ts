70101a35c11c8fbb58d5be5a9a94c4616e79dad4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-13 14:09:40 +0800

    feat: :sparkles: add `Header` component
    - Adds a new `Header` component with navigation and theme switching functionality
    - Includes mobile/desktop versions, avatar, and theme toggle
    - Provides basic structure and styling for the website header

---
+++ apps/web/src/components/header/@types/header.ts
@@ -0,0 +1,79 @@
+import type React from "react";
+
+import type Link from "next/link";
+
+import type {
+  ButtonProps,
+  ButtonRef,
+  DivProps,
+  DivRef,
+  HeaderProps as HeaderComponentProps,
+  HeaderRef as HeaderComponentRef,
+  LiProps,
+  LiRef,
+  SvgProps,
+} from "@guyromellemagayano/components";
+
+/** For common icon props. */
+export interface CommonIconProps extends SvgProps {}
+
+/** For common nav item ref/props. */
+export type CommonNavItemRef = LiRef;
+
+/** For common nav item props. */
+export interface CommonNavItemProps
+  extends LiProps,
+    Pick<React.ComponentPropsWithoutRef<typeof Link>, "target" | "title"> {
+  href?: string;
+}
+
+/** Mobile nav item */
+export type MobileHeaderNavItemRef = CommonNavItemRef;
+
+/** Mobile nav item props */
+export interface MobileHeaderNavItemProps extends CommonNavItemProps {}
+
+/** Desktop nav item */
+export type DesktopHeaderNavItemRef = CommonNavItemRef;
+
+/** Desktop nav item props */
+export interface DesktopHeaderNavItemProps extends CommonNavItemProps {}
+
+/** For mobile nav labels */
+export type MobileHeaderNav = Readonly<Record<string, string>>;
+
+/** Shared links */
+export type HeaderNavLinks = ReadonlyArray<{ label: string; href: string }>;
+
+/** Theme toggle */
+export type ThemeToggleRef = ButtonRef;
+
+/** Theme toggle props */
+export interface ThemeToggleProps extends ButtonProps {}
+
+/** Avatar */
+export type AvatarContainerRef = DivRef;
+
+/** Avatar container props */
+export interface AvatarContainerProps extends DivProps {}
+
+/** Avatar ref */
+export type AvatarRef = HTMLAnchorElement;
+
+/** Avatar props */
+export interface AvatarProps
+  extends React.ComponentPropsWithoutRef<typeof Link> {
+  large?: boolean;
+}
+
+/** Avatar component labels */
+export type AvatarComponentLabels = Readonly<Record<string, string>>;
+
+/** Header ref */
+export type HeaderRef = HeaderComponentRef;
+
+/** Header props */
+export interface HeaderProps extends HeaderComponentProps {
+  /** Opt-in memoization wrapper (for profiling) */
+  isMemoized?: boolean;
+}
