f1eded6ed145ac62fa06e4ecab2bb57f5cf603c8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-13 16:19:32 +0800

    fix(web): :bug: refactor `ArticleLayout` component
    Improves the `ArticleLayout` component by:
    
    - Separating concerns into smaller modules for better organization and maintainability.
    - Exporting the `ArticleLayout` component directly from its own file.
    - Moving internal components, such as icons, into their own directory.
    - Defines types in a separate `models` directory
    - Adds more comprehensive tests, including conditional rendering and ref forwarding

---
+++ apps/web/src/components/layouts/article/models/types.d.ts
@@ -0,0 +1,18 @@
+import type { SvgProps } from "@guyromellemagayano/components";
+
+import type { ContainerProps, ContainerRef } from "@web/components";
+import type { ArticleWithSlug } from "@web/lib";
+
+/** Props for the `ArrowLeftIcon` component. */
+export interface ArrowLeftIconProps extends SvgProps {}
+
+/** Ref for the `ArticleLayout` component. */
+export type ArticleLayoutRef = ContainerRef;
+
+/** Props for the `ArticleLayout` component. */
+export interface ArticleLayoutProps extends ContainerProps {
+  /**
+   * The article to display.
+   */
+  article: ArticleWithSlug;
+}
