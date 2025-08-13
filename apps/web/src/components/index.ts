22a9ac997b9654ff344df7d5ac086c1329ad8cf0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-14 05:10:45 +0800

    chore(web): :hammer: update main component exports
    - Update exports to include new and refactored components for improved maintainability.

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,5 +1,7 @@
 export * from "./card";
 export * from "./container";
+export * from "./footer";
+export * from "./header";
 export * from "./layouts/article";
 export * from "./layouts/simple";
 export * from "./prose";
