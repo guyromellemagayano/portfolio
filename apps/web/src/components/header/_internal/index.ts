9dddb0d52899beb0e909004109e6444980ec20ff
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-15 03:56:31 +0800

    refactor(web): :recycle: update `Header` component with CSS modules
    - Migrate `Header` component to CSS modules for encapsulated styling
    - Remove inline styles in favor of CSS module classes
    - Delete obsolete test files related to previous Header implementation

--- apps/web/src/components/header/_internal/index.ts
+++
@@ -1,8 +0,0 @@
-// Server-side internals
-export * from "./server/HeaderAvatar";
-export * from "./server/HeaderIcons";
-
-// Client-side internals
-export * from "./client/HeaderEffects.client";
-export * from "./client/HeaderNavigation.client";
-export * from "./client/HeaderThemeToggle.client";
