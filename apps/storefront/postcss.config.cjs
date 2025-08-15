5b2dec1f78f064cc80a3225692dac0f150c7a7e8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-15 13:08:37 +0800

    build(web): :package: configure web app for Vercel deployment
    - Add .npmrc for `pnpm` configuration and performance
    - Add .vercelignore to exclude unnecessary files from deployment
    - Add vercel-build.sh for dependency installation and workspace build
    - Add vercel.json to define build command and framework

--- apps/storefront/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "@tailwindcss/postcss": {},
-    "postcss-nesting": {},
-    "postcss-preset-env": {},
-    "postcss-focus-visible": {},
-  },
-};
