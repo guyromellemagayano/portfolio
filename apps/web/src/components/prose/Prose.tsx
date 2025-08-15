117838da3b1528a3ea67ef02d303604e87526fce
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-15 11:22:07 +0800

    feat(web): :sparkles: exports `Prose` component type
    - Export `ProseComponent` type
    - Improve type safety and usage

--- apps/web/src/components/prose/Prose.tsx
+++ apps/web/src/components/prose/Prose.tsx
@@ -1,12 +1,12 @@
 import { Div } from "@guyromellemagayano/components";
 
-import type { ProseProps } from "@web/components/prose";
+import type { ProseComponent } from "@web/components/prose";
 import { cn } from "@web/lib";
 
 import styles from "./Prose.module.css";
 
 /** A component that renders a prose element. */
-export const Prose = function Prose(props: ProseProps) {
+export const Prose: ProseComponent = function Prose(props) {
   const { className, ...rest } = props;
 
   return <Div className={cn(styles.proseContainer, className)} {...rest} />;
