216a2efd35994d9de8ead5dfaa25c8f987a2a98a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-15 10:30:07 +0800

    fix(web): :bug: improve component definitions
    - Uses named function expressions for functional components
    - Improves stack trace readability and debugging
    - Exports new `Base` layout

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -3,5 +3,6 @@ export * from "./container";
 export * from "./footer";
 export * from "./header";
 export * from "./layouts/article";
+export * from "./layouts/base";
 export * from "./layouts/simple";
 export * from "./prose";
