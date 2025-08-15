3bff6e1fc3c35bf51f1c24212277b1f9f661c3af
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-16 05:08:12 +0800

    feat(web): :sparkles: add common `web` app component props
    - Add shared interface for web app component props
    - Define common props: internal ID, debug mode
    - Promote consistency and simplify component development

---
+++ apps/web/src/@types/components.ts
@@ -0,0 +1,7 @@
+/** Common props for all web components */
+export interface CommonWebAppComponentProps {
+  /** Internal ID for the component (hidden from consumers) */
+  _internalId?: string;
+  /** Internal debug mode for the component (hidden from consumers) */
+  _debugMode?: boolean;
+}
