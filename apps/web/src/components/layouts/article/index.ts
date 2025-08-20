2e7b88beb661364372f124257bdbbe3d1329a36b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-20 13:46:53 +0800

    refactor(web): :recycle: update `ArticleLayout` component
    - Moves `ArticleNavButton` into the main `ArticleLayout` component.
    - Consolidates CSS styles into a single module.
    - Introduces internal components for better code organization and testing.
    - Adds comprehensive unit tests for both `ArticleLayout` and `ArticleNavButton` components.

--- apps/web/src/components/layouts/article/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./_internal/client/ArticleNavButton";
-export * from "./_internal/server/ArticleLayoutIcons";
-export * from "./ArticleLayout";
-export * from "./models/ArticleLayout.data";
-export type * from "./models/ArticleLayout.types";
