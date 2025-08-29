d9be98fccf04f4d5bd0b44d281436efc4b65ed81
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-08-29 13:49:59 +0800

    refactor: :recycle: remove `client-counter-button` component and add centralized exports
    - Deleted the `client-counter-button` component as it is no longer needed.
    - Introduced a new `index.ts` file to centralize exports for better organization and maintainability
    of component imports.
    - Updated `ComponentProps` interface in `react.ts` to allow additional props, enhancing flexibility
    for future component enhancements.

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,10 @@
+export { Card } from "./card";
+export { Container } from "./container";
+export { Footer } from "./footer";
+export { Header } from "./header";
+export { Icon } from "./icon";
+export { ArticleLayout } from "./layouts/article";
+export { Layout } from "./layouts/base";
+export { SimpleLayout } from "./layouts/simple";
+export { Prose } from "./prose";
+export { Section } from "./section";
