4939029b2e23508b429c8fb45a44d8d81eee5c5b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-09-17 11:14:58 +0800

    refactor: :recycle: update `ArticleItem` component imports and styles
    - Renamed the import from `Layouts` to `Layout` for consistency with the new component structure.
    - Updated the `ArticleItem` component to use `ARTICLE_ITEM_COMPONENT_LABELS` for improved clarity.
    - Moved the CSS module for `ArticleItem` to a dedicated `styles` directory, enhancing organization.
    - These changes improve the maintainability and clarity of the `ArticleItem` component.

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -3,7 +3,7 @@ export * from "./Container";
 export * from "./Footer";
 export * from "./Header";
 export * from "./Icon";
-export * from "./Layouts";
+export * from "./Layout";
 export * from "./Links";
 export * from "./Prose";
 export * from "./Section";
