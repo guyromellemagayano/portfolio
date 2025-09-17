d431ab8223f9fb0f0018fe74d30393fa5121700a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-09-17 23:17:06 +0800

    refactor(web): :recycle: remove `ArticleItem` component and related files
    - Deleted the `ArticleItem` component along with its associated styles, data, and tests to
    streamline the codebase.
    - This removal enhances maintainability by reducing complexity and focusing on more relevant
    components.

--- apps/web/src/components/ArticleItem/_data/ArticleItem.data.ts
+++
@@ -1,5 +0,0 @@
-type ArticleItemComponentLabels = Readonly<Record<string, string>>;
-
-export const ARTICLE_ITEM_COMPONENT_LABELS = {
-  cta: "Read article",
-} as const satisfies ArticleItemComponentLabels;
---
+++ apps/web/src/components/container/_internal/ContainerOuter/index.ts
@@ -0,0 +1,1 @@
+export { ContainerOuter } from "./ContainerOuter";
