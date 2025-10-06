541bc1e732f43d47ce77abd354f8f0a68ada0162
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-10-07 07:36:13 +0800

    refactor: :recycle: streamline component exports and enhance organization
    - Updated export statements in multiple components to use named exports instead of wildcard exports,
    improving clarity and maintainability.
    - Introduced new data files for the `Header` component, including types and internationalization
    support, enhancing modularity and organization.
    - Removed unused internal files and CSS modules related to the header, streamlining the component
    structure.
    - This refactor aligns with the existing component architecture and promotes better organization
    within the header components.

--- apps/web/src/components/header/_internal/index.ts
+++
@@ -1,8 +0,0 @@
-export { HeaderAvatar } from "./HeaderAvatar";
-export { HeaderAvatarContainer } from "./HeaderAvatarContainer";
-export { HeaderDesktopNav } from "./HeaderDesktopNav";
-export { HeaderDesktopNavItem } from "./HeaderDesktopNavItem";
-export { HeaderEffects } from "./HeaderEffects/HeaderEffects";
-export { HeaderMobileNav } from "./HeaderMobileNav";
-export { HeaderMobileNavItem } from "./HeaderMobileNavItem";
-export { HeaderThemeToggle } from "./HeaderThemeToggle";
