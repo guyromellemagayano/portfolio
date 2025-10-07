02e68853fbc781da4d2f17f08016a2ea4acdf1ca
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-10-07 14:35:30 +0800

    refactor(web): :recycle: enhance icon component structure and improve ARIA support
    - Updated the `Icon` component to allow custom element types via the `as` prop, enhancing
    flexibility.
    - Refactored individual icon components (e.g., `ArrowDownIcon`, `ArrowLeftIcon`, etc.) to utilize
    the new `as` prop for rendering, improving consistency across icons.
    - Introduced comprehensive ARIA attribute support in all icon components, ensuring proper roles and
    labels for accessibility.
    - Added new test files for various icons, validating ARIA attributes and ensuring robust coverage
    for accessibility features.
    - This refactor aligns with existing component architecture and enhances maintainability and
    accessibility across icon components.

--- apps/web/src/components/icon/internal/XIcon.tsx
+++ apps/web/src/components/icon/internal/XIcon.tsx
@@ -6,31 +6,29 @@ import {
   setDisplayName,
 } from "@guyromellemagayano/utils";
 
-import { type CommonIconComponent } from "../../data";
+import { type CommonIconComponent } from "../data";
 
 /** X (Twitter) icon. */
-const XIcon: CommonIconComponent = setDisplayName(
+export const XIcon: CommonIconComponent = setDisplayName(
   React.memo(function XIcon(props) {
-    const { _internalId, _debugMode, ...rest } = props;
+    const { as: Component = "svg", debugId, debugMode, ...rest } = props;
 
-    const { id, isDebugMode } = useComponentId({
-      internalId: _internalId,
-      debugMode: _debugMode,
+    const { componentId, isDebugMode } = useComponentId({
+      debugId,
+      debugMode,
     });
 
     const element = (
-      <svg
+      <Component
         {...rest}
         viewBox="0 0 24 24"
         aria-hidden="true"
-        {...createComponentProps(id, "icon-x-twitter", isDebugMode)}
+        {...createComponentProps(componentId, "icon-x-twitter", isDebugMode)}
       >
         <path d="M13.3174 10.7749L19.1457 4H17.7646L12.7039 9.88256L8.66193 4H4L10.1122 12.8955L4 20H5.38119L10.7254 13.7878L14.994 20H19.656L13.3171 10.7749H13.3174ZM11.4257 12.9738L10.8064 12.0881L5.87886 5.03974H8.00029L11.9769 10.728L12.5962 11.6137L17.7652 19.0075H15.6438L11.4257 12.9742V12.9738Z" />
-      </svg>
+      </Component>
     );
 
     return element;
   })
 );
-
-export { XIcon };
