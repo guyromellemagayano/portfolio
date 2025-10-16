94a5c11b6332223750b08c9d50eb6707b1a753ed
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-10-17 05:23:20 +0800

    chore(web): :hammer: add initial Rantail configuration
    - Introduced a new `rantail.config.js` file to configure Rantail for managing Tailwind CSS styles.
    - The configuration specifies content paths, CSS file location, and other settings to streamline the
    styling process.
    - This addition enhances the project's styling management and aligns with recent updates in the
    Tailwind CSS setup.

---
+++ apps/web/rantail.config.js
@@ -0,0 +1,11 @@
+/** @type {import('rantail').IConfig} */
+const config = {
+  content: ["./src/components/**/*.{tsx}"],
+  cssFilePath: "src/styles/tailwind.css",
+  cuidLength: 5,
+  suffix: "_",
+  prefix: "_",
+  ignore: "_",
+};
+
+export default config;
