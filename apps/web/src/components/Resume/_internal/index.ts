8548dbc78a5c5a988580d6fbd60d5e7d9b11719c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-10-25 03:43:48 +0800

    feat: :sparkles: enhance `Resume` component structure and data management
    - Refactored the `Resume` component to export all related data and internationalization from new
    `Resume.data.ts` and `Resume.i18n.ts` files, centralizing resume-related assets for better
    organization.
    - Updated the main `Resume/index.ts` to use wildcard exports, simplifying the import process for
    consumers of the `Resume` component.
    - This enhancement improves the modularity of the `Resume` component and facilitates easier updates
    to data and internationalization management.

--- apps/web/src/components/Resume/_internal/index.ts
+++ apps/web/src/components/Resume/_internal/index.ts
@@ -1,4 +1,4 @@
-export { ResumeDownloadButton } from "./ResumeDownloadButton";
-export { ResumeRoleList } from "./ResumeRoleList";
-export { ResumeRoleListItem } from "./ResumeRoleListItem";
-export { ResumeTitle } from "./ResumeTitle";
+export * from "./ResumeDownloadButton";
+export * from "./ResumeRoleList";
+export * from "./ResumeRoleListItem";
+export * from "./ResumeTitle";
