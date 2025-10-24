7d793c53444b449dd4ef130b4838113a9b9a1235
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-10-25 05:02:12 +0800

    feat: :sparkles: introduce internationalization for `Article` component
    - Added a new `Article.i18n.ts` file to manage internationalization strings for the `Article`
    component, centralizing action and content labels.
    - Updated the `Article.tsx` file to import the new internationalization constants, improving
    modularity and organization.
    - Removed the old `_data` structure, streamlining the component's data management.
    - This enhancement facilitates easier updates to internationalization and improves the overall
    structure of the `Article` component.

--- apps/web/src/components/Article/index.ts
+++ apps/web/src/components/Article/index.ts
@@ -1,5 +1,1 @@
-export { Article } from "./Article";
-export { ArticleLayout } from "./ArticleLayout";
-export { ArticleList } from "./ArticleList";
-export { ArticleListItem } from "./ArticleListItem";
-export { ArticleNavButton } from "./ArticleNavButton";
+export * from "./Article";
