cc6cae84a54dafc407b64af8c3b71c269610747f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-11-21 17:49:05 +0800

    refactor(web): :recycle: standardize icon usage across components
    - Updated icon usage in various components to utilize the new `name` prop for better consistency and
    clarity.
    - Refactored `ArticleNavButton`, `Card`, `NewsletterForm`, `HeaderMobileNav`, `HeaderThemeToggle`,
    `ResumeDownloadButton`, and `ResumeTitle` components to replace direct icon imports with the
    standardized `Icon` component.
    - This refactor enhances maintainability and improves the overall integration of icons within the
    components.

--- apps/web/src/components/Layout/_internal/AboutPageLayout/AboutPageLayout.tsx
+++ apps/web/src/components/Layout/_internal/AboutPageLayout/AboutPageLayout.tsx
@@ -189,8 +189,9 @@ const BaseAboutPageLayout: AboutPageLayoutComponent = setDisplayName(
                           "mt-8 flex border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
                       )}
                     >
-                      <Link.Social
+                      <Link
                         {...rest}
+                        variant="social"
                         className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
                         debugId={componentId}
                         debugMode={isDebugMode}
