9f2b5982073311760fbb1f1177d37b3a67c67f1e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-12-09 16:14:21 +0800

    chore(api): :hammer: enhance ESLint configuration and clean up server code
    - Updated ESLint configuration to include TypeScript and JavaScript files with appropriate global
    settings.
    - Removed unnecessary ESLint disable comments in `server.ts` for improved code clarity.
    - Streamlined request handling middleware by eliminating redundant type annotations.

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,9 +1,17 @@
+import globals from "globals";
+
 import { baseEslintConfig } from "@guyromellemagayano/config-eslint";
 
 /** @type {import("eslint").Linter.Config} */
 export default [
   ...baseEslintConfig,
   {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
     settings: {
       "import/resolver": {
         typescript: {
