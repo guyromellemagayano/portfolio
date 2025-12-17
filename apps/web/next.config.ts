8234acf86158769a66d0bcecf5a1eaeb281d27ad
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-12-17 16:07:34 +0800

    chore: :hammer: add Next.js configuration and update import paths
    - Introduced a new `next.config.ts` file to configure Next.js with MDX support and custom settings.
    - Updated the `rantail.config.cjs` file to correct the CSS file path.
    - Modified the `vitest.config.ts` to use the updated import path for the React preset from the
    `@guyromellemagayano/vitest-presets` package.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,7 @@
-import rehypePrism from "@mapbox/rehype-prism";
 import nextMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   transpilePackages: [
     "@guyromellemagayano/components",
     "@guyromellemagayano/logger",
@@ -24,10 +22,6 @@ const nextConfig = {
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
 });
 
 export default withMDX(nextConfig);
