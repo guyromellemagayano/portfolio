3f5f7e885a104242b40f116cfaf58e3243d32184
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-12-31 17:15:01 +0800

    refactor(web): :recycle: consolidate `Resume` component structure and remove internal components
    - Merged `Resume.data.ts`, `Resume.i18n.ts`, and internal components into the main `Resume.tsx` file
    for improved organization.
    - Removed unused internal components (`ResumeDownloadButton`, `ResumeRoleList`, `ResumeTitle`, etc.)
    to simplify the codebase.
    - Updated the `Resume` component to utilize a new structure for role data and internationalization.
    - Added integration tests for the `Resume` component to ensure proper rendering and functionality.

---
+++ apps/web/src/images/index.ts
@@ -0,0 +1,33 @@
+import avatarImage from "@web/images/avatar.jpg";
+import logoAirbnb from "@web/images/logos/airbnb.svg";
+import logoAnimaginary from "@web/images/logos/animaginary.svg";
+import logoCosmos from "@web/images/logos/cosmos.svg";
+import logoFacebook from "@web/images/logos/facebook.svg";
+import logoHelioStream from "@web/images/logos/helio-stream.svg";
+import logoOpenShuttle from "@web/images/logos/open-shuttle.svg";
+import logoPlanetaria from "@web/images/logos/planetaria.svg";
+import logoStarbucks from "@web/images/logos/starbucks.svg";
+import image1 from "@web/images/photos/image-1.jpg";
+import image2 from "@web/images/photos/image-2.jpg";
+import image3 from "@web/images/photos/image-3.jpg";
+import image4 from "@web/images/photos/image-4.jpg";
+import image5 from "@web/images/photos/image-5.jpg";
+import portraitImage from "@web/images/portrait.jpg";
+
+export {
+  avatarImage,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  logoAirbnb,
+  logoAnimaginary,
+  logoCosmos,
+  logoFacebook,
+  logoHelioStream,
+  logoOpenShuttle,
+  logoPlanetaria,
+  logoStarbucks,
+  portraitImage,
+};
