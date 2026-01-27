e1e66a07aeead7ab8a6a6928d8abd3d389a22915
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-01-27 11:55:08 +0800

    fix(web): :bug: update locale handling in i18n request configuration
    - Modified the locale retrieval to use cookies for dynamic locale management, defaulting to "en" if
    not set.
    - Updated the import path for messages to ensure correct loading from the new directory structure.

--- apps/web/src/i18n/request.ts
+++ apps/web/src/i18n/request.ts
@@ -1,10 +1,13 @@
+/* eslint-disable simple-import-sort/imports */
 import { getRequestConfig } from "next-intl/server";
+import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
-  const locale = "en";
+  const store = await cookies();
+  const locale = store.get("locale")?.value || "en";
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages: (await import(`@web/messages/${locale}.json`)).default,
   };
 });
