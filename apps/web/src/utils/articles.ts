8b95a94e63a0d7d5a3174d8353cef5d8a64f6e6d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-01 04:56:44 +0800

    refactor: :recycle: enhance article utility functions and improve type definitions
    - Added utility functions for importing articles and retrieving all articles from the articles
    directory, improving modularity and functionality.
    - Updated type definitions for better clarity and maintainability, ensuring consistent handling of
    article data.
    - Included documentation comments for improved understanding of the utility functions.

--- apps/web/src/utils/articles.ts
+++ apps/web/src/utils/articles.ts
@@ -1,3 +1,13 @@
+/**
+ * @file articles.ts
+ * @author Guy Romelle Magayano
+ * @description Utility functions for articles.
+ */
+
+import { type ComponentType } from "react";
+
+import glob from "fast-glob";
+
 export type Article = {
   title: string;
   date: string;
@@ -9,3 +19,31 @@ export type Article = {
 export type ArticleWithSlug = Article & {
   slug: string;
 };
+
+/** Import an article from the articles directory and return it with a slug */
+async function importArticle(
+  articleFilename: string
+): Promise<ArticleWithSlug> {
+  let { article } = (await import(
+    `@web/app/(blog)/articles/${articleFilename}`
+  )) as {
+    default: ComponentType;
+    article: Article;
+  };
+
+  return {
+    slug: articleFilename.replace(/(\/page)?\.mdx$/, ""),
+    ...article,
+  };
+}
+
+/** Get all articles from the articles directory */
+export async function getAllArticles() {
+  let articleFilenames = await glob("*/page.mdx", {
+    cwd: "./src/app/(blog)/articles",
+  });
+
+  let articles = await Promise.all(articleFilenames.map(importArticle));
+
+  return articles.sort((a, z) => +new Date(z.date) - +new Date(a.date));
+}
