b7722646e11d66b6118eebed9055d4746e4844e9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-06 14:19:52 +0800

    feat(web): :sparkles: add utility functions for date formatting and fuzzy search
    - Introduced `datetime.ts` with a function to format dates using a custom format.
    - Added `search.ts` containing a custom React hook for performing fuzzy searches on data using
    Fuse.js, enhancing search capabilities in the application.

---
+++ apps/web/src/utils/search.ts
@@ -0,0 +1,42 @@
+/**
+ * @file search.ts
+ * @author Guy Romelle Magayano
+ * @description Utility functions for search.
+ */
+
+import { useMemo, useState } from "react";
+
+import Fuse, { type FuseOptionKey } from "fuse.js";
+
+type FuzzySearchOptions = Omit<typeof Fuse.config, "keys">;
+
+/** Custom React hook for performing fuzzy search on given data using Fuse.js. */
+export function useFuzzySearch<T extends Record<string, unknown> = {}>(
+  data: T[],
+  options?: FuzzySearchOptions,
+  keys: ReadonlyArray<FuseOptionKey<T>> = ["author", "title", "description"]
+) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Create a fuzzy search instance
+  const fuzzySearch = useMemo(() => {
+    const keysArray = [...keys];
+    const index = Fuse.createIndex(keysArray, data);
+    return new Fuse(data, { ...options, keys: keysArray }, index);
+  }, [data, keys, options]);
+
+  // Perform the search and return the results
+  const results = useMemo(() => {
+    if (!searchQuery.trim()) {
+      return data;
+    }
+
+    return fuzzySearch.search(searchQuery).map((result) => result.item);
+  }, [searchQuery, fuzzySearch, data]);
+
+  return {
+    searchQuery,
+    setSearchQuery,
+    results,
+  };
+}
