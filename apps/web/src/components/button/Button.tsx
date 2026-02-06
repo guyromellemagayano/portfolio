5964fe2d50a8c1bc2f3651b8c4b1eee4c600405b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-06 16:39:16 +0800

    feat(web): :sparkles: enhance `Button` component with new features and accessibility improvements
    - Refactored the `Button` component to support `isDisabled` prop for better state management.
    - Introduced `SkipToMainContentButton` for improved accessibility, allowing users to skip directly
    to main content.
    - Updated integration and unit tests to reflect changes in button behavior and ensure proper
    functionality.
    - Enhanced ARIA attributes for better compliance and user experience.

--- apps/web/src/components/button/Button.tsx
+++ apps/web/src/components/button/Button.tsx
@@ -6,53 +6,75 @@
 
 "use client";
 
-import {
-  type ComponentPropsWithoutRef,
-  type ComponentPropsWithRef,
-} from "react";
+import { type ComponentPropsWithoutRef, type ReactNode, useMemo } from "react";
+
+import * as Headless from "@headlessui/react";
+import { useTranslations } from "next-intl";
 
 import { Link } from "@web/components/link";
+import { COMMON_FOCUS_CLASSNAMES } from "@web/data/common";
 import { cn } from "@web/utils/helpers";
 
-export type ButtonElementType = "button";
+// ============================================================================
+// BUTTON COMPONENT
+// ============================================================================
+
 export type ButtonVariant = "primary" | "secondary";
-
-const BUTTON_VARIANT: Record<ButtonVariant, string> = {
-  primary:
-    "bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70",
-  secondary:
-    "bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70",
-};
-
+export type ButtonElementType = typeof Headless.Button;
 export type ButtonProps<P extends Record<string, unknown> = {}> = Omit<
-  ComponentPropsWithRef<ButtonElementType>,
-  "as"
+  Headless.ButtonProps,
+  "children" | "as" | "href"
 > &
   P & {
     as?: ButtonElementType;
-    variant?: ButtonVariant;
     href?: ComponentPropsWithoutRef<typeof Link>["href"];
+    variant?: ButtonVariant;
+    children?: ReactNode;
+    isDisabled?: boolean;
+    onClick?: () => void;
   };
+
+const COMMON_BUTTON_CLASSNAMES = `inline-flex items-center justify-center gap-x-1.5 rounded-full border ${COMMON_FOCUS_CLASSNAMES[0]} border-transparent px-4 py-2 text-sm font-semibold whitespace-nowrap pointer-events-auto cursor-pointer shadow-lg`;
+const DISABLED_COMMON_BUTTON_CLASSNAMES =
+  "opacity-45 disabled:pointer-events-none disabled:cursor-not-allowed";
+
+const BUTTON_VARIANT = ({
+  isDisabled,
+  className,
+}: Pick<ButtonProps, "isDisabled" | "className">) => ({
+  primary: cn(
+    "bg-zinc-900 text-zinc-50 dark:bg-zinc-50 dark:text-zinc-900 dark:shadow-none",
+    isDisabled
+      ? DISABLED_COMMON_BUTTON_CLASSNAMES
+      : "hover:bg-zinc-800 focus-visible:outline-zinc-800 active:bg-zinc-900 dark:hover:bg-zinc-200 dark:focus-visible:outline-zinc-200 dark:active:bg-zinc-300",
+    className
+  ),
+  secondary: cn(
+    "bg-transparent text-zinc-900 ring-1 ring-zinc-900/10 dark:text-zinc-50 dark:shadow-none dark:ring-zinc-50/10",
+    isDisabled
+      ? DISABLED_COMMON_BUTTON_CLASSNAMES
+      : "hover:bg-zinc-900/10 focus-visible:outline-zinc-600 dark:hover:bg-zinc-50/10 dark:hover:text-zinc-50 dark:focus-visible:outline-zinc-500 dark:active:outline-zinc-400",
+    className
+  ),
+});
 
 export function Button<P extends Record<string, unknown> = {}>(
   props: ButtonProps<P>
 ) {
   const {
-    as: Component = "button",
+    as: Component = Headless.Button,
     variant = "primary",
     className,
     children,
     href,
+    isDisabled = false,
+    onClick,
     ...rest
   } = props;
 
-  if (!children) return null;
-
-  // Common button class name
-  const buttonClassName = cn(
-    "inline-flex items-center justify-center gap-2 rounded-md bg-zinc-800 px-3 py-2 text-sm font-semibold text-zinc-100 outline-offset-2 transition hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 active:transition-none dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70",
-    BUTTON_VARIANT[variant],
-    className
+  const buttonClassNames = cn(
+    COMMON_BUTTON_CLASSNAMES,
+    BUTTON_VARIANT({ isDisabled, className })[variant]
   );
 
   // If href is provided, render a link component instead of a button
@@ -60,8 +82,11 @@ export function Button<P extends Record<string, unknown> = {}>(
     return (
       <Link
         {...(rest as Omit<ComponentPropsWithoutRef<typeof Link>, "href">)}
-        href={href}
-        className={buttonClassName}
+        href={isDisabled ? false : href}
+        tabIndex={isDisabled ? -1 : 0}
+        className={buttonClassNames}
+        disabled={isDisabled}
+        aria-disabled={isDisabled}
       >
         {children}
       </Link>
@@ -71,8 +96,10 @@ export function Button<P extends Record<string, unknown> = {}>(
   return (
     <Component
       {...(rest as ComponentPropsWithoutRef<ButtonElementType>)}
-      role="button"
-      className={buttonClassName}
+      className={buttonClassNames}
+      onClick={onClick}
+      disabled={isDisabled}
+      aria-disabled={isDisabled}
     >
       {children}
     </Component>
@@ -80,3 +107,47 @@ export function Button<P extends Record<string, unknown> = {}>(
 }
 
 Button.displayName = "Button";
+
+// ============================================================================
+// SKIP TO MAIN CONTENT BUTTON COMPONENT
+// ============================================================================
+
+export type SkipToMainContentButtonElementType = typeof Button;
+export type SkipToMainContentButtonProps<
+  P extends Record<string, unknown> = {},
+> = Omit<ButtonProps<P>, "as" | "href"> &
+  P & {
+    as?: SkipToMainContentButtonElementType;
+    href?: ComponentPropsWithoutRef<typeof Link>["href"];
+  };
+
+export function SkipToMainContentButton<P extends Record<string, unknown> = {}>(
+  props: SkipToMainContentButtonProps<P>
+) {
+  const { as: Component = Button, href, className, ...rest } = props;
+
+  // Internationalization
+  const tLabels = useTranslations("layout.labels");
+
+  // Skip to main content ARIA
+  const skipToMainContent = useMemo(
+    () => tLabels("skipToMainContent"),
+    [tLabels]
+  );
+
+  return (
+    <Component
+      {...(rest as ComponentPropsWithoutRef<SkipToMainContentButtonElementType>)}
+      href={href}
+      className={cn(
+        "sr-only focus:not-sr-only focus:fixed focus:top-3 focus:left-3 focus:z-50 focus:px-4 focus:py-2",
+        className
+      )}
+      aria-label={skipToMainContent}
+    >
+      {skipToMainContent}
+    </Component>
+  );
+}
+
+SkipToMainContentButton.displayName = "SkipToMainContentButton";
