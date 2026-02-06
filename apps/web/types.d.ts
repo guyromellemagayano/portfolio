5fbe727a06fbf979c5fe37be496d1a4c35258c1b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-06 14:18:21 +0800

    refactor(web): :recycle: enhance type definitions for internationalization support
    - Added type definitions for `next-intl` to include `Messages` and `Formats`, improving type safety
    for internationalization.
    - Imported localization messages and formats to streamline the integration of internationalization
    features in the application.

--- apps/web/types.d.ts
+++ apps/web/types.d.ts
@@ -1,4 +1,14 @@
+import messages from './messages/en.json';
+import { formats } from './src/i18n/request';
+
 declare module "*.css" {
   const content: any;
   export default content;
+}
+ 
+declare module 'next-intl' {
+  interface AppConfig {
+    Messages: typeof messages;
+    Formats: typeof formats;
+  }
 }
