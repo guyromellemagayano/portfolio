90ce5bfff9f87130ed00d87b07d031c3de351919
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-07 14:18:00 +0800

    feat(web): :sparkles: introduce `List` and `ListItem` components with tests
    - Added `List` and `ListItem` components to enhance list rendering capabilities in the web
    application.
    - Implemented `ArticleList`, `SocialList`, and `ToolsList` as sub-components of `List` for specific
    use cases.
    - Created integration and unit tests for both components to ensure proper rendering and
    functionality.
    - Enhanced accessibility by incorporating ARIA attributes and semantic structure.
    - Exported components through an index file for easier imports.

---
+++ apps/web/src/components/list/List.tsx
@@ -0,0 +1,188 @@
+/**
+ * @file List.tsx
+ * @author Guy Romelle Magayano
+ * @description List component for the web application.
+ */
+
+"use client";
+
+import {
+  type ComponentPropsWithoutRef,
+  type ComponentPropsWithRef,
+  useMemo,
+} from "react";
+
+import { useTranslations } from "next-intl";
+
+import { cn } from "@web/utils/helpers";
+
+// ============================================================================
+// COMMON LIST COMPONENT TYPES
+// ============================================================================
+
+export type ListElementType = "ul" | "ol";
+export type ListProps<P extends Record<string, unknown> = {}> = Omit<
+  ComponentPropsWithRef<ListElementType>,
+  "as"
+> &
+  P & {
+    as?: ListElementType;
+  };
+
+// ============================================================================
+// ARTICLE LIST ITEM COMPONENT
+// ============================================================================
+
+export type ArticleListProps<P extends Record<string, unknown> = {}> =
+  ListProps<P> & P & {};
+
+export function ArticleList<P extends Record<string, unknown> = {}>(
+  props: ArticleListProps<P>
+) {
+  const {
+    as: Component = "ul",
+    role = "region",
+    children,
+    className,
+    ...rest
+  } = props;
+
+  // Internationalization
+  const tLabels = useTranslations("list.labels");
+
+  // Article list ARIA
+  const LIST_I18N = useMemo(
+    () => ({
+      articleList: tLabels("articleList"),
+      articles: tLabels("articles"),
+    }),
+    [tLabels]
+  );
+
+  if (!children) return null;
+
+  return (
+    <Component
+      {...(rest as ComponentPropsWithoutRef<ListElementType>)}
+      role={role}
+      className={cn(
+        "md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40",
+        className
+      )}
+      aria-label={LIST_I18N.articleList}
+    >
+      <h2 className="sr-only">{LIST_I18N.articleList}</h2>
+      <div className="flex w-full max-w-3xl flex-col space-y-16">
+        {children}
+      </div>
+    </Component>
+  );
+}
+
+ArticleList.displayName = "ArticleList";
+
+// ============================================================================
+// SOCIAL LIST ITEM COMPONENT
+// ============================================================================
+
+export type SocialListProps<P extends Record<string, unknown> = {}> =
+  ListProps<P> & P & {};
+
+export function SocialList<P extends Record<string, unknown> = {}>(
+  props: SocialListProps<P>
+) {
+  const { as: Component = "ul", role = "region", children, ...rest } = props;
+
+  // Internationalization
+  const tLabels = useTranslations("list.labels");
+
+  // Social list ARIA
+  const LIST_I18N = useMemo(
+    () => ({
+      socialList: tLabels("socialList"),
+    }),
+    [tLabels]
+  );
+
+  if (!children) return null;
+
+  return (
+    <Component
+      {...(rest as ComponentPropsWithoutRef<ListElementType>)}
+      role={role}
+      aria-label={LIST_I18N.socialList}
+    >
+      {children}
+    </Component>
+  );
+}
+
+SocialList.displayName = "SocialList";
+
+// ============================================================================
+// TOOLS LIST ITEM COMPONENT
+// ============================================================================
+
+export function ToolsList<P extends Record<string, unknown> = {}>(
+  props: ListProps<P>
+) {
+  const {
+    as: Component = "ul",
+    role = "region",
+    children,
+    className,
+    ...rest
+  } = props;
+
+  // Internationalization
+  const tLabels = useTranslations("list.labels");
+
+  // Tools list ARIA
+  const LIST_I18N = useMemo(
+    () => ({
+      toolsList: tLabels("toolsList"),
+    }),
+    [tLabels]
+  );
+
+  if (!children) return null;
+
+  return (
+    <Component
+      {...(rest as ComponentPropsWithoutRef<ListElementType>)}
+      role={role}
+      className={cn("space-y-16", className)}
+      aria-label={LIST_I18N.toolsList}
+    >
+      {children}
+    </Component>
+  );
+}
+
+ToolsList.displayName = "ToolsList";
+
+// ============================================================================
+// MAIN LIST COMPONENT
+// ============================================================================
+
+export function List<P extends Record<string, unknown> = {}>(
+  props: ListProps<P>
+) {
+  const { as: Component = "ul", children, role = "list", ...rest } = props;
+
+  if (!children) return null;
+
+  return (
+    <Component
+      {...(rest as ComponentPropsWithoutRef<ListElementType>)}
+      role={role}
+    >
+      {children}
+    </Component>
+  );
+}
+
+List.displayName = "List";
+List.Article = ArticleList;
+List.Social = SocialList;
+List.Tools = ToolsList;
