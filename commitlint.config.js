f5ee470d4f5df1e156403763be0d142eea605358
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-19 17:01:25 +0800

    refactor(tooling): enhance `commitlint` configuration by adding scope-empty rule
    - Introduced the "scope-empty" rule to the `commitlint` configuration to enforce non-empty scopes in commit messages, improving commit message quality and consistency.

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,6 +22,7 @@ const config = {
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
     "scope-case": [2, "always", "lower-case"],
+    "scope-empty": [2, "never"],
     "subject-case": [
       2,
       "never",
