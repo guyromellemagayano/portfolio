7806ba4632a39c348a595e6db6cd707beb1079d9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2026-02-25 05:47:25 +0800

    refactor(web): move `RouteShell` into components composition
    - move `RouteShell` into `apps/web/src/components/route-shell`
    - add component exports and tests for the new component location
    - update app layout usage to consume the componentized `RouteShell`
    - align placement with web component composition conventions

---
+++ apps/web/src/components/route-shell/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @file apps/web/src/components/route-shell/index.ts
+ * @author Guy Romelle Magayano
+ * @description Public exports for the RouteShell component.
+ */
+
+export * from "./RouteShell";
